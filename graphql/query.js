b04af482f7e0ec26e09a6f98c691a4bb4ddfffff
vongkeo <vongkeokeosavanh@gmail.com>
2021-02-24 18:17:48 +0700

    graphql

--- graphql/query.js
+++ graphql/query.js
@@ -1,19 +1,25 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
   type Query {
-    provinces: [Provinces],
-  },
+    provinces: [Provinces!]
+    districts: [Districts!]
+  }
 
   type Mutation {
     world: String
   }
-    # province model
-  type Provinces{
-    id:Int,
-    pr_name:String,
-    pr_name_en:String,
-}
+  # province model
+  type Provinces {
+    id: Int
+    pr_name: String
+    pr_name_en: String
+  }
+  type Districts {
+    id: Int
+    dr_name: String
+    dr_name_en: String
+    province: Int
+  }
 `;
-module.exports = typeDefs
+module.exports = typeDefs;
